f4e7944cf0d6ff72e0aca909e344e08b73214d75
깃MunChangBin <vv4704@gmail.com>
2023-03-08 12:27:23 +0900

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //int a = 5;
+        //int b = 10;
+        //int c = a;
+        //a = b;
+        //b = c;
+        //System.out.println(a);
+        //System.out.println(b);
+        //20<=age <=34
+        int age = 50;
+        System.out.println("당신의 나이 : " + age);
+        if(age >= 20 && age <=34){
+            System.out.println("청년이네요?");
+        }else
+            System.out.println("미성년이네요?");
+        }
     }
-}
+
+
+
+
